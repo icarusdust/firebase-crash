61ee851f6b5f5478d31dc844483b0e482d5cf2a7
Nihal Agazade <43378371+icarusdust@users.noreply.github.com>
2022-03-19 23:10:07 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+Future<void> main() async {
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://e5af5ef422c247b1888bde8e5d43e00c@o1165433.ingest.sentry.io/6267311';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(MyApp()),
+  );
+
+  // or define SENTRY_DSN via Dart environment variable (--dart-define)
+}  
 
 void main() {
   runApp(MyApp());
